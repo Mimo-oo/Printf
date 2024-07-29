22ac2a989b476dca4b3641601dba109c240be2f5
mimo-oo <fadipemichael6@gmail.com>
2024-07-29 13:21:40 -0700

    correction

--- main.h
+++ main.h
@@ -4,7 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
 
 int _printf(const char *format, ...);
 
