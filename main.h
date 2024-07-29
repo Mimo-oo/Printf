85fea85250e71203a83bf0c7b64d2c9b2b954ff8
mimo-oo <fadipemichael6@gmail.com>
2024-07-29 13:50:34 -0700

    functions

--- main.h
+++ main.h
@@ -39,13 +39,24 @@ struct fmt
 
 typedef struct fmt fmt_t;
 
-/* FUNCTIONS */
+
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
+/* ALL FUNCTIONS */
 
+/* functions to print chars and strings */
+int print_char(va_list types, char buffer [], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer [], int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-
-
+/* fuctions to print numbers */
+int print_int(va_list types, char buffer[], int flags, int width, int precison, int size);
+int print_binary(va_list types, char buffer[], int flags, int width, int precison, int size);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precison, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precison, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precison, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precison, int size);
 
 
 
