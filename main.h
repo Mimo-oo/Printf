b4f1fb9168c7e82e43f649fa2ec0789331774427
mimo-oo <fadipemichael6@gmail.com>
2024-07-29 14:22:34 -0700

    main.h update

--- main.h
+++ main.h
@@ -85,10 +85,11 @@ int print_reverse(va_list types, char buffer[], int flags, int width, int precis
 int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 /* width handler */
-
-
-
-
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, int lenght, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 
 
 
