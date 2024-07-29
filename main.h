d2af25e287331cb5d211f5ee938699b172316f8d
mimo-oo <fadipemichael6@gmail.com>
2024-07-29 13:57:49 -0700

    functions

--- main.h
+++ main.h
@@ -57,7 +57,10 @@ int print_unsigned(va_list types, char buffer[], int flags, int width, int preci
 int print_octal(va_list types, char buffer[], int flags, int width, int precison, int size);
 int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precison, int size);
 int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precison, int size);
+int print_hexa(va_list types, char map_to[],  char buffer[], int flags, char flag_ch, int width, int precison, int size);
 
+/* function to print non printable characters */
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precison, int size);
 
 
 
