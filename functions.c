d028a7506f72130c089f6dc0062d8b5f13b39a06
mimo-oo <fadipemichael6@gmail.com>
2024-07-30 15:08:19 -0700

    functions

--- functions.c
+++ functions.c
@@ -72,6 +72,48 @@ int print_string(va_list types, char buffer[], int flags, int width, int precisi
 	return (write(1, str, length));
 }
 
+/**
+ * print_int - print int
+ * @types: list of arguments
+ * @buffer: buffer array to handle print
+ * @flags calculate active flags
+ * @width: the width
+ * @precision: precision specification
+ * @size: size specifier
+ * Return: Number of int printed
+ */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+	{
+		buffer[i--] = '0';
+	}
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
 
 
 
