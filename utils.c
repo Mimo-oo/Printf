109527e8ec9c98d5ddcb77f933a5fb3324509305
mimo-oo <fadipemichael6@gmail.com>
2024-07-31 13:37:13 -0700

    hexa code

---
+++ utils.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * is_printable - evaluates if a char is printable
+ * @c: char to be evaluated
+ * Return: 1 if c is printable or 0 if its not printable
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append ascii in hexadecimal code
+ * @buffer: array of chars
+ * @i: index at which to start appending
+ * @ascii_code: ASCII CODE
+ * Return: always 3
+ */
+itn append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
