0e29f4b212686dc71baff8623d16e3adf969bc54
mimo-oo <fadipemichael6@gmail.com>
2024-07-31 10:44:00 -0700

    functions

---
+++ functions2.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_pointer - prits the value of a pointer variable
+ * @types: list of arguements
+ * @buffer: buffer array to handle print
+ * @flags: calculates active flags
+ * @width: get width
+ * @precision: precision specification
+ * @size: size specifer
+ * Return: number of 
+ */
