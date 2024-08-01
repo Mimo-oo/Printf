1d83f1e8f57a283ec2f79280940c399dc32605fc
Fadipe Michael <123783232+Mimo-oo@users.noreply.github.com>
2024-08-01 14:46:08 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			if (buff_ind++ == BUFF_SIZE)
+			if (buff_ind == BUFF_SIZE)
 			{
 				print_buffer(buffer, &buff_ind);
 			}
